71a05b76860408e4bec976c5128042be97d9ef0a
Carlos Barajas <12barajasc@gmail.com>
2015-01-04 21:41:20 -0500

    Added 'Chat' Class

--- chatparser.py
+++ chatparser.py
@@ -2,6 +2,7 @@ class Person:
     def __init__(self, name):
         self.name = name
         self.contents = []
+
     def __str__(self):
         return self.name
 
@@ -26,8 +27,18 @@ class Person:
     def add(self, item):
         self.contents.append(item)
 
+class Chat:
+    def __init__(self, items):
+        self.time = items[0][11:]
+        self.contents = items[-1]
+        self.category = items[2]
+        self.ident = items[3]
+        self.toon = items[4]
+
+    def __str__(self):
+
 def cparse(filename):
     players = []
-    newfile = open(filename,'r')
+    newfile = open(filename,'r',encoding='utf-16')
     for line in newfile:
         line = line.strip().split('\t')
