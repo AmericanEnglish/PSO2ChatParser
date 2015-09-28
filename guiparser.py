a953bdbc5b33bf0ab481ac977cd757fda867f146
Carlos Barajas <12barajasc@gmail.com>
2015-09-28 19:05:29 -0400

    Some intial setup

--- guiparser.py
+++ guiparser.py
@@ -1,5 +1,11 @@
-import tkinter
-import tkinter.filedialog as dialog
-import tkinter.messagebox as mbox
-from chatparser import *
-window = tkinter.Tk()
+# Will be cleaner when finished, for not just use it all
+from tkinter import *
+
+# Mulit DB Support
+import psycopg2
+import sqlite3
+
+class GUI:
+    def __init__(self):
+        self.root = Tk()
+        self.master = root.frame()
