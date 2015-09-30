03f31d70bdb174969afaf0d21679c0d83253baf2
Carlos Barajas <12barajasc@gmail.com>
2015-09-30 12:23:30 -0400

    Mental Structuring

--- guiparser.py
+++ guiparser.py
@@ -7,5 +7,14 @@ import sqlite3
 
 class GUI:
     def __init__(self):
+        # Ground Work
         self.root = Tk()
         self.master = root.frame()
+
+        # Setup Visuals
+
+    # Parsing Commands
+
+    # Database Command Util
+
+    # Displaying Chat
