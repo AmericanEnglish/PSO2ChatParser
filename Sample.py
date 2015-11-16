405961d6eaa742e1b122c35e2eca4560e7d0a971
Carlos Barajas <12barajasc@gmail.com>
2015-11-16 14:29:33 -0500

    Testing SQL implementation for chat logs

---
+++ Sample.py
@@ -0,0 +1,25 @@
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+from sys import argv
+import psycopg2
+import sqlite3
+
+username = argv[1]
+password = argv[2]
+
+try:
+    print("Try: 1")
+    conn = psycopg2.connect(host="localhost", database="pso2chat",
+                            user=username, password=password)
+    cur = conn.cursor()
+    print("Try: 2")
+except psycopg2.OperationalError:
+    print("Except: 1")
+    conn = psycopg2.connect(host="localhost", database="postgres", user=username, password=password)
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cur = conn.cursor()
+    cur.execute("CREATE DATABASE pso2chat;")
+    cur.close()
+    conn.close()
+    conn = psycopg2.connect(host="localhost", database="pso2chat", user=username, password=password)
+    cur = conn.cursor()
+    print("Except: 2")
