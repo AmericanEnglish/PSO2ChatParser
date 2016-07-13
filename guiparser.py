c03c9fdb7dcc0989a0cd3e318471acfaf72c05a3
Carlos Barajas <12barajasc@gmail.com>
2016-07-13 16:17:54 -0400

    setting some goals

--- guiparser.py
+++ guiparser.py
@@ -2,14 +2,21 @@ from PyQt5.QtWidgets import QApplication, QWidget, QGridLayout
 import sys
 
 # Mulit DB Support
-import psycopg2
-import sqlite3
+from database import DB
+from os import listdir
 
 class GUI(QWidget):
     def __init__(self):
         # Ground Work
         super().__init__() 
         self.initGui()
+
+        # Detect if a PSOChat.db exists
+            # Check if defaults table exists
+            # Else create defaults table
+        # Else check for a defaults.db
+            # Check defaults if not
+            # Else create one and collect
 
     # Setup Visuals
     def initGui(self):
@@ -19,12 +26,43 @@ class GUI(QWidget):
         self.resize(200, 200)
         self.show()
 
+    def prompt_for_chat(self):
+        # Ask for chat directory
+        # Store directory into defaults table
+        pass
 
-    # Parsing Commands
+    def scan_for_new(self):
+        # Scan directory for new files to be imported
+        # Compare hashed file contents
+            # If filename and hash do not match
+            # Prompt for dropping old and importing new
+        pass
 
-    # Database Command Util
+    def add_new_file(self):
+        # Add new file to the database
+        pass
 
-    # Displaying Chat
+    def display_query_results(self):
+        # Query database
+            # Display results in meaningful way
+
+    # Buttons that concatenate the string to make a query
+    # SID       search
+        # Pull down checkboxes
+    # PID       search
+        # Pull down checkboxes
+    # Name      search
+        # Pull down checkboxes
+    # Keyword   search
+        # Fill in blank
+    # Day       filter
+        # Calendar Widget
+    # Time      filter
+        # Time slider 00:00:00 -> 23:59:59
+    # Chat Type filter
+        # Checkboxes
+
+
 
 
 if __name__ == '__main__':
